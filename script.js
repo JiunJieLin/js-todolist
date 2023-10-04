9c97578f1e7d6f5561ac0d75324ccb6639bb2621
林君潔 <linjunjie@linjunjiedeMacBook-Air.local>
2023-10-04 21:47:54 +0800

    addTodos

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const btn = document.querySelector(".btn-plus");
+
+const inputbar = document.querySelector(".inputbar");
+
+const tasklist = document.querySelector(".tasklist");
+
+const addItem = () => {
+  if (inputbar.value.trim().length === 0) return;
+  const newTodos = document.createElement("li");
+  newTodos.innerHTML = `
+    <div class="task" >
+      <input  class="tasklist-checkbox" type="checkbox">
+      <p class="tasklist-p">${inputbar.value}</p>
+      <button class="btn-delete">+</button>
+      </div>
+    `;
+  tasklist.appendChild(newTodos);
+  inputbar.value = "";
+};
